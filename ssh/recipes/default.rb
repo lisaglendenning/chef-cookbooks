b0f6bc391c99cfcee00e4ca2fd2c494be3cf20e6
Lisa Glendenning <lisaglendenning@seagull.konsume.org>
2010-07-03 19:42:58 -0700

    debugging

--- ssh/recipes/default.rb
+++ ssh/recipes/default.rb
@@ -1,24 +1,2 @@
-#
-# Namespace
-#
-
-#
-# Supported Platforms
-#
-
-rhels = ['redhat', 'centos', 'fedora']
-
-#
-# Default properties
-#
-
-default[:components][:ssl][:packages] = case node[:platform]
-    when rhels
-      ['openssh-clients', 'openssh']
-    else
-      ['openssh-client', 'openssh-server']
-    end
-
-default[:components][:ssl][:server][:transports] = [[:tcp, 22]]
 
 include_recipe "ssh::default_enable"
