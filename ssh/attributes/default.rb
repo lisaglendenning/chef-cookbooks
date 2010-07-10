d82c0dd088d4b2a61ba5c380658c02ad89ad1734
Lisa Glendenning <lisaglendenning@seagull.konsume.org>
2010-07-09 18:27:13 -0700

    added denyhosts package for ssh security

--- ssh/attributes/default.rb
+++ ssh/attributes/default.rb
@@ -1,14 +1,14 @@
 
-default[:components][:ssh][:packages] = case node[:platform]
-  when 'redhat', 'centos', 'fedora'
-    ['openssh-clients', 'openssh',  'openssh-server']
-  else
-    ['openssh-client', 'openssh-server']
-  end
-
 default[:components][:ssh][:root] = true  
 default[:components][:ssh][:auth] = [ 'password', 'publickey' ]
-  
+
+default[:components][:ssh][:packages] = case node[:platform]
+  when 'redhat', 'centos', 'fedora'
+    ['openssh-clients', 'openssh',  'openssh-server', 'denyhosts']
+  else
+    ['openssh-client', 'openssh-server', 'denyhosts']
+  end  
+    
 server = Mash.new
 server[:protocol] = 'tcp'
 server[:port] = 22
