a6cb0a12fa7905e1e4acd4a3a0091704563fb0a5
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-22 13:06:04 -0700

    adding repo import

--- packages/recipes/default.rb
+++ packages/recipes/default.rb
@@ -14,17 +14,40 @@ when 'redhat', 'centos', 'fedora'
   }
   
   repofiles = []
-  Dir.entries(node[:components][:packages][:repodir]).each { |f|
+  repodir = node[:components][:packages][:repodir]
+  Dir.entries(repodir).each { |f|
     if f[-5..-1] == '.repo'
       repofiles << f
     end
   }
   
-  repofiles.each { |f|
-    reponame = f[0..-6]
+  repofiles.each { |fname|
+    reponame = fname[0..-6]
     if ! node[:components][:packages][:repos][:official].key?(reponame)
       if ! node[:components][:packages][:repos][:extra].key?(reponame)
-        node[:components][:packages][:repos][:extra][reponame] = Mash.new
+        repo = Mash.new
+        node[:components][:packages][:repos][:extra][reponame] = repo
+
+        f = File.new("#{repodir}/#{fname}", "r")
+        section = nil    
+        f.each_line do |line|
+          line = line.strip
+          comment = line.index('#')
+          if comment
+            line = line[0...comment]
+          end
+          if line.empty?
+            continue
+          end
+          if line =~ /^\[(.+)\]/
+            section = Mash.new
+            repo[$1.strip] = section
+          elsif line =~ /^(.+?)\s*=\s*(.+)/
+            section[$1.strip] = $2.strip
+          else
+            # FIXME: error
+          end          
+        f.close
       end
     end
   }
