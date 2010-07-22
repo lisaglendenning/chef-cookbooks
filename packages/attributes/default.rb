445f1c346a3289a93875d0a3cfa019c9f65e2050
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-22 10:30:40 -0700

    Added Centos repos

--- packages/attributes/default.rb
+++ packages/attributes/default.rb
@@ -1,6 +1,39 @@
 case node[:platform]
 when 'redhat', 'centos', 'fedora'
   set[:components][:packages][:repodir] = '/etc/yum.repos.d'
+    
+  if node[:platform] == 'centos'
+    release = 'CentOS-5'
+    
+    base_repo = Mash.new
+    [['base', 'Base', 'os'], 
+     ['updates', 'Updates', 'updates'],
+     ['addons', 'Addons', 'addons'],
+     ['extras', 'Extras', 'extras'],
+     ['centosplus', 'Plus', 'centosplus'],
+     ['contrib', 'Contrib', 'contrib']].each { |r|
+      base_repo[r[0]] = Mash.new( 
+        :name => "CentOS-$releasever - #{r[1]}",
+        :mirrorlist => "http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=#{r[2]}",
+        #:baseurl => "http://mirror.centos.org/centos/$releasever/#{r[2]}/$basearch/"
+        :gpgcheck => "1",
+        :gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-#{release}",
+        :enabled => "1"
+      )
+    }
+    
+    media_repo = Mash.new
+    media_repo['c5-media'] = Mash.new(
+      :name => "CentOS-$releasever - Media",
+      :baseurl => "file:///media/CentOS/\nfile:///media/cdrom/\nfile:///media/cdrecorder/",
+      :gpgcheck => "1",
+      :gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-#{release}",
+      :enabled => "0"
+      ) 
+
+    default[:components][:packages][:repos][:official]['CentOS-Base'] = base_repo 
+    default[:components][:packages][:repos][:official]['CentOS-Media'] = media_repo
+  end
 else
   set[:components][:packages][:repodir] = '/etc/apt'
   
--- packages/recipes/default.rb
+++ packages/recipes/default.rb
@@ -1,5 +1,18 @@
 case node[:platform]
 when 'redhat', 'centos', 'fedora'
+  node[:components][:packages][:repos][:official].each { |k,v|
+    template k do
+      path node[:components][:packages][:repodir] + "/#{k}.repo"
+      source "repo.erb"
+      mode 0644
+      owner "root"
+      group "root"
+      variables(
+        :repo => v
+      )
+    end
+  }
+  
 else
   template 'sources.list' do
     path node[:components][:packages][:repodir] + "/sources.list"
