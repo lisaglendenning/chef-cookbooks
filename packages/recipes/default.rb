c3a766e56351cd8f5b1704059494a1eb447ffaee
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-24 15:29:04 -0700

    fix

--- packages/recipes/default.rb
+++ packages/recipes/default.rb
@@ -28,8 +28,8 @@ when 'redhat', 'centos', 'fedora'
   
   node[:components][:packages][:plugins].each { |k,v|
     yum_plugin k do
-      :plugin v[:plugin]
-      :action v[:action]
+      plugin v[:plugin]
+      action v[:action]
     end
   }
   
