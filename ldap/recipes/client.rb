b4b364cc7a24316e43aaab2b3921cd7609a1438b
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-25 06:07:43 -0700

    fix

--- ldap/recipes/client.rb
+++ ldap/recipes/client.rb
@@ -29,7 +29,7 @@ if node.components.ldap.client.attribute?(:cert)
       }
     end
     only_if "[ -f #{fname} ]"
-    action (node.components.ssl.attribute?(certregistry) && node.components.ssl.certregistry.attribute?(certname)) ? :nothing : :create
+    action (node.components.ssl.attribute?(:certregistry) && node.components.ssl.certregistry.attribute?(certname)) ? :nothing : :create
   end
   remote_file fname do
     path fname
