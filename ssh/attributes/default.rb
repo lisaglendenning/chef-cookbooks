c1b98b754b217511dec5fc925abf842c87ff892f
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-25 00:15:40 -0700

    fixing attributes

--- ssh/attributes/default.rb
+++ ssh/attributes/default.rb
@@ -1,15 +1,13 @@
 
-default[:components][:ssh][:root] = true  
-default[:components][:ssh][:auth] = [ 'password', 'publickey' ]
+node.default[:components][:ssh][:server][:enabled] = true 
+node.default[:components][:ssh][:server][:root] = true  
+node.default[:components][:ssh][:server][:auth] = ['password', 'publickey']
 
-default[:components][:ssh][:packages] = case node[:platform]
+node.default[:components][:ssh][:packages] = case node[:platform]
   when 'redhat', 'centos', 'fedora'
     ['openssh-clients', 'openssh',  'openssh-server', 'denyhosts']
   else
     ['openssh-client', 'openssh-server', 'denyhosts']
   end  
-    
-server = Mash.new
-server[:protocol] = 'tcp'
-server[:port] = 22
-default[:components][:ssh][:server][:transports] = [server]
+
+node.default[:components][:ssh][:server][:port] = 22
--- ssh/recipes/default.rb
+++ ssh/recipes/default.rb
@@ -1,2 +1,48 @@
 
-include_recipe "ssh::default_enable"
+#
+# Resources
+#
+
+node[:components][:ssh][:packages].each { |p|
+  package p do
+   action :upgrade
+  end
+}
+
+CONFDIR = '/etc/ssh'
+CONFFILE = CONFDIR + '/sshd_config'
+
+service 'sshd' do
+  case node[:platform]
+  when 'redhat', 'centos', 'fedora'
+    service_name "sshd"
+  else
+    service_name "ssh"
+  end
+  supports :restart => true, :status =>true
+  action node[:components][:ssh][:server][:enabled] ? [:enable, :start] : [:disable, :stop]
+end
+
+template 'sshd-config' do
+  path CONFFILE
+  source "sshd_config.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => 'sshd')
+  parameters(
+    :server => node[:components][:ssh][:server],
+    :platform => node[:platform]
+    )
+end
+
+if node.components.attribute?(:firewall)
+  server = Mash.new(:protocol => 'tcp', :port => node[:components][:ssh][:server][:port])
+  node.set[:components][:firewall][:registry][:sshd] = [server]
+end
+
+service 'denyhosts' do
+  service_name 'denyhosts'
+  supports :restart => true, :status =>false
+  action [:enable, :start]
+end
