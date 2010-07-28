d880d8b012295ce42bf1417435d0b7733ebf122c
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-27 20:38:09 -0700

    fix

--- perl/files/default/cpan.py
+++ perl/files/default/cpan.py
@@ -9,23 +9,25 @@ CONF = 'Are you ready for manual configuration? [yes]'
 PROMPT = 'cpan> '
 
 def main(argv):
-    child = pexpect.spawn('cpan')
-    i = child.expect_exact([CONF, PROMPT])
-    if i == 0:
-        child.sendline('no')
-        child.expect(PROMPT)
-    for cmd in argv[1:]:
-        child.sendline(cmd)
-        while True:
-            i = child.expect_exact(['[yes]', PROMPT])
-            if i == 0:
-                child.sendline('')
-            else:
-                break
-    child.expect(PROMPT)
-    child.sendline('quit')
-    child.expect(pexpect.EOF)
-    child.close()
+    # need a longer timeout for cpan
+    child = pexpect.spawn('cpan', timeout=300, logfile=sys.stdout)
+    try:
+        i = child.expect_exact([CONF, PROMPT])
+        if i == 0:
+            child.sendline('no')
+            child.expect(PROMPT)
+        for cmd in argv[1:]:
+            child.sendline(cmd)
+            while True:
+                i = child.expect_exact(['[yes]', PROMPT])
+                if i == 0:
+                    child.sendline('')
+                else:
+                    break
+        child.sendline('quit')
+        child.expect(pexpect.EOF)
+    finally:
+        child.close()
 
 if __name__ == '__main__':
     main(sys.argv)
