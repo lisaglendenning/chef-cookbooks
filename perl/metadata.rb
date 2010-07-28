9d21a905cf18468a2fb2298f5d4ba393d1413a45
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-27 21:57:04 -0700

    fix

---
+++ perl/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "cs.washington.edu"
+maintainer_email "lglenden@cs.washington.edu"
+license          "Apache 2.0"
+description      "Perl"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.2"
+depends          "packages"
