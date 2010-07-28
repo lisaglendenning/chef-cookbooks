0d79ae1ab75a53d0f3f9daf3faa6d7c2df82d0ef
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-27 21:22:38 -0700

    fix

--- perl/files/default/cpan.py
+++ perl/files/default/cpan.py
@@ -1,6 +1,11 @@
 #!/bin/env python
 
-import sys, re
+"""Expect script for CPAN.
+
+Takes a list of commandline arguments, which are passed in sequence to cpan.
+"""
+
+import sys, os, re
 
 # must be installed
 import pexpect
@@ -8,17 +13,27 @@ import pexpect
 CONF = r'Are you ready for manual configuration? \[yes\]'
 PROMPT = r'cpan> '
 MAKE_ERROR = r'\nmake: \*\*\* \[.+\] Error \d+'
+CONFLICT_ERROR = r'\nOther job is running.\n'
 UNCLEAN_ERROR = r'Other job not responding. Shall I overwrite the lockfile? \(Y/N\) \[y\]'
+KILL_MATCH = r'\s*kill (\d+)\s*'
+
+def cpan():
+    # need a longer timeout for cpan than 30s
+    return pexpect.spawn('cpan', timeout=120, logfile=sys.stdout)
 
 def main(argv):
-    # need a longer timeout for cpan
-    child = pexpect.spawn('cpan', timeout=300, logfile=sys.stdout)
+    child = cpan()
     try:
         while True:
-            i = child.expect([UNCLEAN_ERROR, CONF, PROMPT])
+            i = child.expect([CONFLICT_ERROR, UNCLEAN_ERROR, CONF, PROMPT])
             if i == 0:
-                child.sendline('')
+                pid = re.search(KILL_MATCH, child.after).group(1)
+                os.system('kill %s' % pid)
+                child.close()
+                child = cpan()
             elif i == 1:
+                child.sendline('')
+            elif i == 2:
                 child.sendline('no')
             else:
                 break
