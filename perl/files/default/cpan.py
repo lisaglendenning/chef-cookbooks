6daad0abb715b8b8e59a24f9226a4ca11b3fddaf
Lisa Glendenning <lglenden@cs.washington.edu>
2010-07-27 20:25:32 -0700

    fix

--- perl/files/default/cpan.py
+++ perl/files/default/cpan.py
@@ -5,19 +5,27 @@ import sys
 # must be installed
 import pexpect
 
+CONF = 'Are you ready for manual configuration? [yes]'
 PROMPT = 'cpan> '
 
 def main(argv):
     child = pexpect.spawn('cpan')
-    i = child.expect(['Are you ready for manual configuration? [yes] ', PROMPT])
+    i = child.expect_exact([CONF, PROMPT])
     if i == 0:
         child.sendline('no')
         child.expect(PROMPT)
-    else:
-        for cmd in argv[1:]:
-            child.sendline(cmd)
-            child.expect(PROMPT)
-        child.sendline('quit')
+    for cmd in argv[1:]:
+        child.sendline(cmd)
+        while True:
+            i = child.expect_exact(['[yes]', PROMPT])
+            if i == 0:
+                child.sendline('')
+            else:
+                break
+    child.expect(PROMPT)
+    child.sendline('quit')
+    child.expect(pexpect.EOF)
+    child.close()
 
 if __name__ == '__main__':
     main(sys.argv)
