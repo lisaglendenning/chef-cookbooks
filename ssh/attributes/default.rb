fbeae5cd729a7a8ef7e75013ec26541f3f835411
Lisa Glendenning <lglenden@cs.washington.edu>
2010-12-01 16:19:06 -0800

    added users

--- ssh/attributes/default.rb
+++ ssh/attributes/default.rb
@@ -4,3 +4,4 @@ node.default[:components][:ssh][:server][:root] = true
 node.default[:components][:ssh][:server][:auth] = ['password', 'publickey']
 node.default[:components][:ssh][:server][:port] = 22
 node.default[:components][:ssh][:server][:denyhosts] = true 
+node.default[:components][:ssh][:server][:allowusers] = []
