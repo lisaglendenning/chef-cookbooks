7705c4cd3ca3bdddca28e3270421ad6808477f2c
Lisa Glendenning <lglenden@cs.washington.edu>
2010-12-03 12:16:30 -0800

    sites

--- django/recipes/default.rb
+++ django/recipes/default.rb
@@ -3,7 +3,18 @@
 case node[:platform]
 when 'redhat', 'centos', 'fedora'
   # Requires EPEL
-  package 'django' do
+  package 'Django' do
     action :upgrade
   end
 end
+
+# initialize sites
+root = node[:components][:django][:root]
+node[:components][:django][:sites].each { |site,props|
+  execute "django-admin #{site}" do
+    command "django-admin startproject #{site}"
+    cwd root
+    creates "#{root}/#{site}" 
+    action :run   
+  end
+}
