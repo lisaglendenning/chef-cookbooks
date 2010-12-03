3ff8d2ed4f1a1dd1a6b89bbe074979611cbf5a6f
Lisa Glendenning <lglenden@cs.washington.edu>
2010-12-03 12:25:36 -0800

    fix

---
+++ django/attributes/default.rb
@@ -0,0 +1,5 @@
+
+node.default[:components][:django][:root] = '/var/local/django'
+node.default[:components][:django][:sites] = {}
+node.default[:components][:django][:user] = 'nobody'
+node.default[:components][:django][:group] = 'nobody'
--- django/recipes/default.rb
+++ django/recipes/default.rb
@@ -9,7 +9,17 @@ when 'redhat', 'centos', 'fedora'
 end
 
 # initialize sites
+
 root = node[:components][:django][:root]
+
+directory root do
+  path root
+  owner node[:components][:django][:user]
+  group node[:components][:django][:group]
+  mode "0750"
+  recursive true
+end
+    
 node[:components][:django][:sites].each { |site,props|
   execute "django-admin #{site}" do
     command "django-admin startproject #{site}"
