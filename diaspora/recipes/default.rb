fe443a23654d8c230f7b36337e5c6116178f47f5
Lisa Glendenning <lglenden@cs.washington.edu>
2010-12-28 14:02:44 -0800

    rvm

--- diaspora/recipes/default.rb
+++ diaspora/recipes/default.rb
@@ -1,5 +1,5 @@
 
-# requirements
+# Requirements
 
 case node[:platform]
 when 'redhat', 'centos', 'fedora'
@@ -20,5 +20,35 @@ when 'redhat', 'centos', 'fedora'
     package p do
       action :upgrade
     end
+  end
+end
+
+# Ruby 1.87
+
+case node[:platform]
+when 'redhat', 'centos', 'fedora'
+  packages = [
+    'git'
+  ]
+  
+  packages.each do |p|
+    package p do
+      action :upgrade
+    end
+  end
+  
+  # RVM
+  
+  remote_file "rvm-install-head" do
+    path "/tmp/rvm-install-head"
+    source "http://rvm.beginrescueend.com/releases/rvm-install-head"
+    mode "0755"
+    backup false
+  end
+  
+  execute "rvm-install-head" do
+    command "/tmp/rvm-install-head"
+    action :nothing
+    subscribes :run, resources(:remote_file => "rvm-install-head")
   end
 end
