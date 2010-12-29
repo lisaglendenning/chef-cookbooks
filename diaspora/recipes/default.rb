968bc9f52bfaf7eb36ce372e75f2def213b0fb3b
Lisa Glendenning <lglenden@cs.washington.edu>
2010-12-28 21:03:53 -0800

    fix

--- diaspora/recipes/default.rb
+++ diaspora/recipes/default.rb
@@ -188,7 +188,7 @@ execute "diaspora-install" do
 end
 
 template "app_config.yml" do
-  path "#{diaspora[:root]}source/diaspora.git/config/app_config.yml"
+  path "#{diaspora[:root]}/source/diaspora.git/config/app_config.yml"
   source "app_config.yml.erb"
   mode "0644"
   owner diaspora[:user]
