dad351c24e28ad6bcfb307e1fe3657f1dc01a0ad
Lisa Glendenning <lglenden@cs.washington.edu>
2010-12-31 11:52:05 -0800

    fix

--- webserver/attributes/default.rb
+++ webserver/attributes/default.rb
@@ -2,3 +2,4 @@
 node.default[:components][:webserver][:registry] = Mash.new
 node.default[:components][:webserver][:provider] = :nginx
 node.default[:components][:webserver][:user] = :nobody
+node.default[:components][:webserver][:group] = :nobody
